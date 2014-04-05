5439b84826025663aea80ff0ca4bc4cdf586bb70
Stefan Dasbach <stefandasbach@gmail.com>
2014-04-05 13:48:40 -0400

    Add audio chat and visualization

---
+++ audioClient.py
@@ -0,0 +1,57 @@
+from matplotlib.mlab import find
+import numpy as np
+import math
+import socket
+import pyaudio
+import wave
+import time
+
+#record
+CHUNK = 1024
+FORMAT = pyaudio.paInt16
+CHANNELS = 1
+RATE = 44100
+RECORD_SECONDS = 40
+
+HOST = ''    # The remote host
+PORT = 50007              # The same port as used by the server
+
+def Pitch(signal):
+    signal = np.fromstring(signal, 'Int16');
+    crossing = [math.copysign(1.0, s) for s in signal]
+    index = find(np.diff(crossing));
+    f0=round(len(index) *RATE /(2*np.prod(len(signal))))
+    return f0;
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect((HOST, PORT))
+
+p = pyaudio.PyAudio()
+
+stream = p.open(format=FORMAT,
+                channels=CHANNELS,
+                rate=RATE,
+                input=True,
+                frames_per_buffer=CHUNK)
+
+print("*recording")
+
+frames = []
+
+for i in range(0, int(RATE/CHUNK*RECORD_SECONDS)):
+    data = stream.read(CHUNK)
+    Frequency=Pitch(data)
+    amplitude=(int)(Frequency/50.0)
+    print "|"*amplitude
+    
+    frames.append(data)
+    s.sendall(data)
+
+print("*done recording")
+
+stream.stop_stream()
+stream.close()
+p.terminate()
+s.close()
+
+print("*closed")
---
+++ audioServer.py
@@ -0,0 +1,55 @@
+import socket
+import pyaudio
+import wave
+import time
+
+CHUNK = 1024
+FORMAT = pyaudio.paInt16
+CHANNELS = 1
+RATE = 44100
+RECORD_SECONDS = 4
+WAVE_OUTPUT_FILENAME = "server_output.wav"
+WIDTH = 2
+frames = []
+
+
+def Pitch(signal):
+    signal = np.fromstring(signal, 'Int16');
+    crossing = [math.copysign(1.0, s) for s in signal]
+    index = find(np.diff(crossing));
+    f0=round(len(index) *RATE /(2*np.prod(len(signal))))
+    return f0;
+
+p = pyaudio.PyAudio()
+stream = p.open(format=p.get_format_from_width(WIDTH),
+                channels=CHANNELS,
+                rate=RATE,
+                output=True,
+                frames_per_buffer=CHUNK)
+
+
+HOST = ''                 # Symbolic name meaning all available interfaces
+PORT = 50007              # Arbitrary non-privileged port
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.bind((HOST, PORT))
+s.listen(1)
+conn, addr = s.accept()
+print 'Connected by', addr
+data = conn.recv(1024)
+
+while data != '':
+    stream.write(data)
+    data = conn.recv(1024)
+    frames.append(data)
+
+wf = wave.open(WAVE_OUTPUT_FILENAME, 'wb')
+wf.setnchannels(CHANNELS)
+wf.setsampwidth(p.get_sample_size(FORMAT))
+wf.setframerate(RATE)
+wf.writeframes(b''.join(frames))
+wf.close()
+
+stream.stop_stream()
+stream.close()
+p.terminate()
+conn.close()
