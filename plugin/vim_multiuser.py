c144fa37241fe3f0842bc876ed8f4a312534dc56
Bram <bwasti@gmail.com>
2014-04-06 02:53:23 -0400

    added new lines

--- plugin/vim_multiuser.py
+++ plugin/vim_multiuser.py
@@ -57,7 +57,7 @@ def multiuser_client_send():
                 to_send['line'] = current_buffer[i]
                 to_send['insert'] = i
                 break
-            elif ((i == len(current_buffer)-1 and len(current_buffer) len(old_buffer)+1):
+            elif (i == len(current_buffer)-1 and len(current_buffer) == len(old_buffer)+1):
                 to_send['line'] = current_buffer[i]
                 to_send['insert'] = i
             elif ((i != len(old_buffer)-1 and current_buffer[i:] == old_buffer[i+1:])
